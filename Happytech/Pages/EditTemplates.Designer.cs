084fef9d3d0dbe35b92771fb5baf48201ecf834b
Charlotte Causton <65340340+CCauston113@users.noreply.github.com>
2020-12-11 10:48:01 +0000

    Some progress with UpdateTemplate method - almost stores everything in Template object

--- Happytech/Pages/EditTemplates.Designer.cs
+++ Happytech/Pages/EditTemplates.Designer.cs
@@ -102,6 +102,7 @@
             this.button1.TabIndex = 3;
             this.button1.Text = "UPDATE FIELDS";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.UpdateTemplate);
             // 
             // button2
             // 
